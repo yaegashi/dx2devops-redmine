37c016bb41e5a18c6531844ea3e6172643376859
YAEGASHI Takeshi <yaegashi@gmail.com>
2022-05-24 17:52:54 +0000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,41 +1,55 @@
 #!/bin/bash
 
 WWWROOT=/home/site/wwwroot
+STATICSITEDIR=$WWWROOT/staticsite
+BACKUPSDIR=$WWWROOT=backups
 FILESDIR=$WWWROOT/files
-MAINTDIR=$WWWROOT/maint
+PLUGINSDIR=$WWWROOT/plugins
+PUBLICTHEMESDIR=$WWWROOT/public/themes
+PUBLICPLUGINASSETSDIR=$WWWROOT/public/plugin_assets
 
 set -ex
-
-abort() {
-    set +x
-    mkdir -p $MAINTDIR
-    echo "Maintenance mode: $*" >$MAINTDIR/index.html
-    cat $MAINTDIR/index.html >&2
-    echo "Starting maintenance server..." >&2
-    ruby -run -e httpd $MAINTDIR -p 8080
-    exit $?
-}
-
-if test $# -gt 0; then
-    "$@"
-    exit $?
-fi
 
 if test -n "$DATABASE_URL" -a ! -e config/database.yml ; then
     echo '{"0":{"adapter":"'${DATABASE_URL%%:*}'"}}' >config/database.yml
 fi
 
-mkdir -p $FILESDIR $MAINTDIR
-rm -rf files
-ln -s $FILESDIR files
+if test -z "$SECRET_KEY_BASE"; then
+    export SECRET_KEY_BASE=$(ruby -rsecurerandom -e 'puts SecureRandom.hex(64)')
+fi
 
-mkdir /root/.ssh
+mkdir -p $FILESDIR $PUBLICPLUGINASSETSDIR
+rm -rf files public/plugin_assets
+ln -snf $FILESDIR files
+ln -snf $PUBLICPLUGINASSETSDIR public/plugin_assets
+
+for i in $PLUGINSDIR/*; do
+    test -d $i || continue
+    ln -snf $i plugins/${i##*/}
+done
+
+for i in $PUBLICTHEMESDIR/*; do
+    test -d $i || continue
+    ln -snf $i public/themes/${i##*/}
+done
+
+mkdir -p /root/.ssh
 env >/root/.ssh/environment
-
 /usr/sbin/sshd
 
-if test -n "$RAILS_IN_SERVICE"; then
-    rails server -b 0.0.0.0 -p 8080 || abort "Failed to start server: $?"
+set +x
+if test $# -gt 0; then
+    echo "Running command line: $@"
+    "$@"
+elif test -n "$RAILS_IN_SERVICE"; then
+    echo "Rails in service: true" >&2
+    echo "Starting rails server..." >&2
+    rm -f tmp/pids/server.pid
+    rails server -b 0.0.0.0 -p 8080
 else
-    abort "RAILS_IN_SERVICE is not set"
+    echo "Rails in service: false" >&2
+    echo "Starting static site server..." >&2
+    mkdir -p $STATICSITEDIR
+    echo "Maintenance mode" >$STATICSITEDIR/index.html
+    ruby -run -e httpd $STATICSITEDIR -p 8080
 fi
